e6d7e26c1bdd823045f47d1e0b82ab84dec4dc0d
Sviataslau <ushev.s90@gmail.com>
2022-08-09 16:00:51 +0200

    added ssl logic

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.get('/', (req, res) => {
 const httpsServer = https.createServer(options, app);
 
 httpsServer.listen(5001, () => {
-  logger.info(`Server started on port 5001`);
+  console.log(`Server started on port 5001`);
 });
 
 const httpServer = http.createServer(options, app);
 httpServer.listen(5000, () => {
-  logger.info(`Server started on port 5000`);
+  console.log(`Server started on port 5000`);
 });
