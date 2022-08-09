7c2c7d03a74d0fb7194207d708243e56bb565aeb
Sviataslau <ushev.s90@gmail.com>
2022-08-09 15:40:19 +0200

    added ssl logic

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>My Node App</h1>');
+const options = {
+  cert: fs.readFileSync('./ssl/svushev_click.crt'),
+  ca: fs.readFileSync('./ssl/svushev_click.ca-bundle'),
+  key: fs.readFileSync('./ssl/svushev_click.rsa.key'),
+};
+
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
 });
 
-app.listen(5000, () => {
-  console.log('App listening on port 5000!');
+app.get('/', (req, res) => {
+  res.send('<h1>NodeJS App 1</h1>');
+});
+
+const httpsServer = https.createServer(options, app);
+
+httpsServer.listen(5001, () => {
+  logger.info(`Server started on port 5001`);
+});
+
+const httpServer = http.createServer(options, app);
+httpServer.listen(5000, () => {
+  logger.info(`Server started on port 5000`);
 });
